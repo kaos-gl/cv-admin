38a57a2df7cc6eb6e5dc30d56ebada622b68b225
kaos <kaxel1200@gmail.com>
2026-08-24 18:17:31 -0600

    http solution creo

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,6 +5,7 @@ import { provideFirestore, getFirestore } from '@angular/fire/firestore';
 import { provideAuth, getAuth } from '@angular/fire/auth';
 import { routes } from './app.routes';
 import { environment } from '../environments/environment';
+import { provideHttpClient } from '@angular/common/http'; // ¡NUEVO: Importación agregada!
 
 export const appConfig: ApplicationConfig = {
   providers: [
@@ -12,5 +13,6 @@ export const appConfig: ApplicationConfig = {
     provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
     provideFirestore(() => getFirestore()),
     provideAuth(() => getAuth()),
+    provideHttpClient(), // ¡NUEVO: Proveedor agregado para que Cloudinary funcione!
   ],
 };
